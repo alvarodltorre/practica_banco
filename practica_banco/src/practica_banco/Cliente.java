6796ca8aebb04c9a2fe0204ff793405008f02f47
Zryderace <147004178+Zryderace@users.noreply.github.com>
2024-04-30 17:11:46 +0200

    getters setters

--- practica_banco/src/practica_banco/Cliente.java
+++ practica_banco/src/practica_banco/Cliente.java
@@ -12,4 +12,34 @@ public class Cliente {
 		this.edad = edad;
 	}
 	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String getApellidos() {
+		return apellidos;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
 }
---
+++ practica_banco/src/practica_banco/Main.java
@@ -0,0 +1,12 @@
+package practica_banco;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Cliente cliente1 = new Cliente("paco", "de la torre", 87);
+		Cliente cliente2 = new Cliente("alvaro", "redanion", 54);
+
+	}
+
+}
