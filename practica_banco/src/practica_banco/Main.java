a94d6423e29628ce41ed53c5f77f8355ef0f0f12
Zryderace <147004178+Zryderace@users.noreply.github.com>
2024-04-30 17:32:28 +0200

    main cliente y cuentas

--- practica_banco/src/practica_banco/Main.java
+++ practica_banco/src/practica_banco/Main.java
@@ -7,6 +7,9 @@ public class Main {
 		Cliente cliente1 = new Cliente("paco", "de la torre", 87);
 		Cliente cliente2 = new Cliente("alvaro", "redanion", 54);
 
+		Cuenta cuenta1 = new Cuenta("123ABC", cliente1, 50000);
+		Cuenta cuenta2 = new Cuenta("456ZYX", cliente2, 90000);
+		
 		
 		
 	}
