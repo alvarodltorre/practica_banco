1c75f24c7e508eaefadf55367d6a6cd69b0428c5
PROTH3X <edualvaro138@gmail.com>
2024-04-30 17:42:08 +0200

    Clase Operaciones acabada

--- practica_banco/src/practica_banco/Operaciones.java
+++ practica_banco/src/practica_banco/Operaciones.java
@@ -1,8 +1,18 @@
 package practica_banco;
 
 public class Operaciones {
-	//public void transferencia (Cuenta cuentaOrigen, Cuenta CuentaDestino, int cantidad) {
-		
-	//}
+	public void transferencia (Cuenta cuentaOrigen, Cuenta cuentaDestino, double cantidad) {
+		cuentaOrigen.setCantidad(cuentaOrigen.getCantidad()-cantidad);
+		cuentaDestino.setCantidad(cuentaDestino.getCantidad()+cantidad);
+	}
+	
+	public void prestamo (Cuenta cuentaDestino, double cantidad) {
+		cuentaDestino.setCantidad(cuentaDestino.getCantidad()+cantidad);
+	}
+	
+	public void cancelarCuenta (Cuenta cuentaDestino) {
+		cuentaDestino.setEstado(false);
+	}
+	
 
 }
