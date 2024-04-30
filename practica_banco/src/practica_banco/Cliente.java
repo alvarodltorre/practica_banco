74f69b4d7d1b78ece3cbd0f8e5b48fe3303aab58
Zryderace <147004178+Zryderace@users.noreply.github.com>
2024-04-30 17:06:38 +0200

    clase cliente alex

--- practica_banco/src/practica_banco/Cliente.java
+++ practica_banco/src/practica_banco/Cliente.java
@@ -2,4 +2,14 @@ package practica_banco;
 
 public class Cliente {
 
+	private String nombre;
+	private String apellidos;
+	private int edad;
+	
+	public Cliente(String nombre, String apellidos, int edad) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+	}
+	
 }
